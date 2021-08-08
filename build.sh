c8352a8dd6773cb123174d2a20173085486a8601
Shami Ivan <shamiivan@gmail.com>
2021-08-07 22:03:40 -0400

    Add build script for dev mode

---
+++ build.sh
@@ -0,0 +1,45 @@
+mkdir webscraping
+cd webscraping
+
+npm config ls -l 
+
+npm config set init-main "index.js"
+npm config set init-author-name "Shami Ivan"
+npm config set init-version "1.0.0"
+npm config set init-license "MIT"
+
+npm init -y 
+
+npm install puppeteer
+
+touch index.js
+
+# cat ../../Documents/programs/webscraping/index.js >> index.js 
+echo "t puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  await page.screenshot({ path: 'example.png' });
+
+  await browser.close();
+})();
+
+
+//const puppeteer = require('puppeteer');
+//
+//(async () => {
+//  const browser = await puppeteer.launch({
+//        executablePath: "/usr/bin/chromium-browser"
+//          });
+//            const page = await browser.newPage();
+//              await page.goto('https://example.com');
+//                await page.screenshot({ path: 'example.png' });
+//
+//                  await browser.close();
+//
+})();
+
+
+" >> index.js
